a3960947a2c898ae6a1ea3c8358c59569f2ed460
Gabriel Linhares <145071752+linharesg@users.noreply.github.com>
2023-11-14 13:12:47 -0300

    Update filtros.js
    hotfix: reestruturação da função e correção do filtro quando outro filtro é aplicado

--- static/js/filtros.js
+++ static/js/filtros.js
@@ -1,115 +1,38 @@
-const filterMarca = document.querySelector('.filtros .marca input')
+// Função genérica para filtrar os cards com base nos inputs e nas classes desejadas
+function filterCards(filters, cards) {
+    for (const card of cards) {
+        let shouldDisplay = true;
 
-const cards = document.querySelectorAll('.anuncios_box li')
+        for (const filter of filters) {
+            const title = card.querySelector(`.${filter.cardClass}`);
+            const titleText = title.textContent.toLowerCase();
 
-filterMarca.addEventListener('input', filterCards)
-
-function filterCards() {
-
-    if(filterMarca.value != '') {
-        for (let card of cards) {
-            let title = card.querySelector('.Marca')
-            title = title.textContent.toLowerCase()
-
-            let filterText = filterMarca.value.toLowerCase()
-
-            if(!title.includes(filterText)) {
-                card.style.display = "none"
-            }
-            else {
-                card.style.display = "block"
+            if (filter.input.value !== '' && !titleText.includes(filter.input.value.toLowerCase())) {
+                shouldDisplay = false;
+                break;
             }
         }
-    } else {
-        for (let card of cards) {
-            card.style.display = "block"
-        }
+
+        card.style.display = shouldDisplay ? 'block' : 'none';
     }
 }
 
-const filterModelo = document.querySelector('.filtros .modelo input')
-
-const cards1 = document.querySelectorAll('.anuncios_box li')
-
-filterModelo.addEventListener('input', filterCards1)
-
-function filterCards1() {
-
-    if(filterModelo.value != '') {
-        for (let card of cards1) {
-            let title = card.querySelector('.modelo')
-            title = title.textContent.toLowerCase()
-
-            let filterText = filterModelo.value.toLowerCase()
-
-            if(!title.includes(filterText)) {
-                card.style.display = "none"
-            }
-            else {
-                card.style.display = "block"
-            }
-        }
-    } else {
-        for (let card of cards1) {
-            card.style.display = "block"
-        }
-    }
+// Adiciona o listener de evento para cada filtro
+function addFilterEventListener(filter, filters) {
+    filter.input.addEventListener('input', () => {
+        filterCards(filters, filter.cards);
+    });
 }
 
-const filterano = document.querySelector('.filtros .ano input')
+// Configuração dos filtros
+const filtersConfig = [
+    { input: document.querySelector('.filtros .marca input'), cards: document.querySelectorAll('.anuncios_box li'), cardClass: 'Marca' },
+    { input: document.querySelector('.filtros .modelo input'), cards: document.querySelectorAll('.anuncios_box li'), cardClass: 'modelo' },
+    { input: document.querySelector('.filtros .ano input'), cards: document.querySelectorAll('.anuncios_box li'), cardClass: 'ano' },
+    { input: document.querySelector('.filtros .preco input'), cards: document.querySelectorAll('.anuncios_box li'), cardClass: 'preco' },
+];
 
-const cards2 = document.querySelectorAll('.anuncios_box li')
-
-filterano.addEventListener('input', filterCards2)
-
-function filterCards2() {
-
-    if(filterano.value != '') {
-        for (let card of cards2) {
-            let title = card.querySelector('.ano')
-            title = title.textContent.toLowerCase()
-
-            let filterText = filterano.value.toLowerCase()
-
-            if(!title.includes(filterText)) {
-                card.style.display = "none"
-            }
-            else {
-                card.style.display = "block"
-            }
-        }
-    } else {
-        for (let card of cards2) {
-            card.style.display = "block"
-        }
-    }
-}
-
-const filterpreco = document.querySelector('.filtros .preco input')
-
-const cards3 = document.querySelectorAll('.anuncios_box li')
-
-filterpreco.addEventListener('input', filterCards3)
-
-function filterCards3() {
-
-    if(filterpreco.value != '') {
-        for (let card of cards3) {
-            let title = card.querySelector('.preco')
-            title = title.textContent.toLowerCase()
-
-            let filterText = filterpreco.value.toLowerCase()
-
-            if(!title.includes(filterText)) {
-                card.style.display = "none"
-            }
-            else {
-                card.style.display = "block"
-            }
-        }
-    } else {
-        for (let card of cards3) {
-            card.style.display = "block"
-        }
-    }
+// Adiciona o listener de evento para cada filtro usando a configuração
+for (const filterConfig of filtersConfig) {
+    addFilterEventListener(filterConfig, filtersConfig);
 }
